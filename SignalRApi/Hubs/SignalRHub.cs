98dceb26318514e8517e1e429bb9182d0d4a4215
halilselvitopu <halilselvitopu@gmail.com>
2024-10-14 04:31:57 +0300

    Consume live statistics with signalR

--- SignalRApi/Hubs/SignalRHub.cs
+++ SignalRApi/Hubs/SignalRHub.cs
@@ -1,16 +1,75 @@
 ﻿using Microsoft.AspNetCore.SignalR;
-using SignalR.DataAccessLayer.Concrete;
+using SignalR.BusinessLayer.Abstract;
 
 namespace SignalRApi.Hubs
 {
 	public class SignalRHub : Hub
 	{
-		SignalRContext context = new SignalRContext();
+		private readonly ICategoryService _categoryService;
+		private readonly IProductService _productService;
+		private readonly IOrderService _orderService;
+		private readonly ITableService _tableService;
+		private readonly ICashRegisterService _cashRegisterService;
 
-		public async Task SendCategoryCount()
+		public SignalRHub(ICategoryService categoryService, IProductService productService, IOrderService orderService, ITableService tableService, ICashRegisterService cashRegisterService)
 		{
-			var value = context.Categories.Count();
-			await Clients.All.SendAsync("ReceiveCategoryCount",value);
+			_categoryService = categoryService;
+			_productService = productService;
+			_orderService = orderService;
+			_tableService = tableService;
+			_cashRegisterService = cashRegisterService;
 		}
+
+		public async Task SendStatistics()
+		{
+			var categoryCount = _categoryService.TGetCategoryCount();
+			await Clients.All.SendAsync("ReceiveCategoryCount", categoryCount);
+
+			var productCount = _productService.TGetProductCount();
+			await Clients.All.SendAsync("ReceiveProductCount", productCount);
+
+			var activeCategoryCount = _categoryService.TGetActiveCategoryCount();
+			await Clients.All.SendAsync("ReceiveActiveCategoryCount", activeCategoryCount);
+
+			var passiveCategoryCount = _categoryService.TGetActiveCategoryCount();
+			await Clients.All.SendAsync("ReceivePassiveCategoryCount", passiveCategoryCount);
+
+			var hamburgerCount = _productService.TGetProductCountByCategoryNameHamburger();
+			await Clients.All.SendAsync("ReceiveHamburgerCount", hamburgerCount);
+
+			var drinkCount = _productService.TGetProductCountByCategoryNameHamburger();
+			await Clients.All.SendAsync("ReceiveDrinkCount", drinkCount);
+
+			var avergaePrice = _productService.TGetAverageProductPrice();
+			await Clients.All.SendAsync("ReceiveAveragePrice", avergaePrice.ToString("0.00") + "₺");
+
+			var expensiveProduct = _productService.TGetMostExpensiveProductNames();
+			await Clients.All.SendAsync("ReceiveExpensiveProduct", expensiveProduct);
+
+			var cheapestProduct = _productService.TGetCheapestProductNames();
+			await Clients.All.SendAsync("ReceiveCheapestProduct", cheapestProduct);
+
+			var averageHamburgerPrice = _productService.TGetAverageHamburgerPrice();
+			await Clients.All.SendAsync("ReceiveAverageHamburgerPrice", averageHamburgerPrice.ToString("0.00") + "₺");
+
+			var orderCount = _orderService.TGetTotalOrderCount();
+			await Clients.All.SendAsync("ReceiveTotalOrderCount", orderCount);
+
+			var activeOrderCount = _orderService.TGetActiveOrderCount();
+			await Clients.All.SendAsync("ReceiveActiveOrderCount", activeOrderCount);
+
+			var lastOrderPrice = _orderService.TGetLastOrderPrice();
+			await Clients.All.SendAsync("ReceiveLastOrderPrice", lastOrderPrice.ToString("0.00") + "₺");
+
+			var cashRegisterPrice = _cashRegisterService.TGetTotalPriceCashRegister();
+			await Clients.All.SendAsync("ReceiveTotalCashRegisterPrice", cashRegisterPrice.ToString("0.00") + "₺");
+
+			var todayProfit = _orderService.TGetTodayTotalProfit();
+			await Clients.All.SendAsync("ReceiveTodayProfit", todayProfit.ToString("0.00") + "₺");
+
+			var tableCount = _tableService.TGetTotalTableCount();
+			await Clients.All.SendAsync("ReceiveTableCount", tableCount);
+		}
+
 	}
 }
