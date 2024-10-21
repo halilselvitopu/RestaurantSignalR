bfdc63a1c66f63a7a178e24e52b379a079f0d391
halilselvitopu <halilselvitopu@gmail.com>
2024-10-21 19:57:01 +0300

    contact us section finished

--- SignalR.DtoLayer/MessageDto/GetMessageDto.cs
+++ SignalR.DtoLayer/MessageDto/GetMessageDto.cs
@@ -12,6 +12,7 @@ namespace SignalR.DtoLayer.MessageDto
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+        public string PhoneNumber { get; set; }
         public string Subject { get; set; }
         public string Content { get; set; }
         public DateTime Date { get; set; }
