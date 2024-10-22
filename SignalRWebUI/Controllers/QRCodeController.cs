b7136adff63e2aa81769c4c8870ccdd6ffdc9c9d
halilselvitopu <halilselvitopu@gmail.com>
2024-10-22 23:33:24 +0300

    QR Code DLL, controller and generate QR Code completed

---
+++ SignalRWebUI/Controllers/QRCodeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using SkiaSharp;
+
+namespace SignalRWebUI.Controllers
+{
+	public class QRCodeController : Controller
+	{
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Index(string value)
+		{
+			using (MemoryStream memoryStream = new MemoryStream())
+			{
+				QRCodeGenerator createQRCode = new QRCodeGenerator();
+				QRCodeData qrCodeData = createQRCode.CreateQrCode(value, QRCodeGenerator.ECCLevel.Q);
+
+				
+				PngByteQRCode qrCode = new PngByteQRCode(qrCodeData);
+				byte[] qrCodeBytes = qrCode.GetGraphic(10);
+
+				
+				ViewBag.QrCodeImage = "data:image/png;base64," + Convert.ToBase64String(qrCodeBytes);
+			}
+
+			return View();
+		}
+	}
+}
